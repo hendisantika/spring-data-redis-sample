00c54f8934e506458a930ad41091b09bf58b39aa
Hendi Santika <hendisantika@gmail.com>
2017-12-24 21:27:41 +0700

    #12 - Modify User entity

--- src/main/java/com/hendisantika/rediscache/entity/User.java
+++ src/main/java/com/hendisantika/rediscache/entity/User.java
@@ -52,5 +52,13 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
 
