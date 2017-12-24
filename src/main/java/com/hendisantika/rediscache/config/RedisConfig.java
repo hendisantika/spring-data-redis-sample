3562dbb7a27a4e5a8756c8b85564349fb64bfd9c
Hendi Santika <hendisantika@gmail.com>
2017-12-24 21:27:23 +0700

    #11 - Modify Redis Config

--- src/main/java/com/hendisantika/rediscache/config/RedisConfig.java
+++ src/main/java/com/hendisantika/rediscache/config/RedisConfig.java
@@ -1,7 +1,15 @@
 package com.hendisantika.rediscache.config;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +26,30 @@ import org.springframework.context.annotation.Configuration;
 // Spring boot redisConnectionFactory,redisTemplate,cacheManager
 @EnableCaching
 public class RedisConfig {
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.setUsePool(true);
+        return jedisConnectionFactory;
+    }
+
+    @Bean
+    public RedisSerializer redisStringSerializer() {
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        return stringRedisSerializer;
+    }
+
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf, RedisSerializer redisSerializer) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
+        redisTemplate.setConnectionFactory(cf);
+        redisTemplate.setDefaultSerializer(redisSerializer);
+        return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new RedisCacheManager(redisTemplate(redisConnectionFactory(), redisStringSerializer()));
+    }
 }
