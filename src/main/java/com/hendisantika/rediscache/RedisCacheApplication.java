bc211f7c24f5637562fb9892f01d163a86dea6bf
Hendi Santika <hendisantika@gmail.com>
2017-12-24 21:08:29 +0700

    #4 - Modify Main App

--- src/main/java/com/hendisantika/rediscache/RedisCacheApplication.java
+++ src/main/java/com/hendisantika/rediscache/RedisCacheApplication.java
@@ -2,9 +2,16 @@ package com.hendisantika.rediscache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RedisCacheApplication {
+public class RedisCacheApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RedisCacheApplication.class);
+    }
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RedisCacheApplication.class, args);
