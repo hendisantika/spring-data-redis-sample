ebbc547fa2cbb4586dd2cb0ca49ee6b73459acb8
Hendi Santika <hendisantika@gmail.com>
2017-12-24 21:09:51 +0700

    #5 - Add Redis Config

---
+++ src/main/java/com/hendisantika/rediscache/config/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.hendisantika.rediscache.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : redis-cache
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/12/17
+ * Time: 21.08
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Configuration
+// Spring boot redisConnectionFactory,redisTemplate,cacheManager
+@EnableCaching
+public class RedisConfig {
+}
