23a5e46d667c0ffb34185fcda7388d28ff447918
Hendi Santika <hendisantika@gmail.com>
2018-10-06 09:11:44 +0700

    #13 - Upgraded into Spring Boot 1.5.16.RELEASE version

--- src/main/java/com/hendisantika/rediscache/config/RedisConfig.java
+++ src/main/java/com/hendisantika/rediscache/config/RedisConfig.java
@@ -1,15 +1,9 @@
 package com.hendisantika.rediscache.config;
 
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * Created by IntelliJ IDEA.
@@ -34,22 +28,22 @@ public class RedisConfig {
         return jedisConnectionFactory;
     }
 
-    @Bean
-    public RedisSerializer redisStringSerializer() {
-        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        return stringRedisSerializer;
-    }
-
-    @Bean(name = "redisTemplate")
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf, RedisSerializer redisSerializer) {
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
-        redisTemplate.setConnectionFactory(cf);
-        redisTemplate.setDefaultSerializer(redisSerializer);
-        return redisTemplate;
-    }
-
-    @Bean
-    public CacheManager cacheManager() {
-        return new RedisCacheManager(redisTemplate(redisConnectionFactory(), redisStringSerializer()));
-    }
+//    @Bean
+//    public RedisSerializer redisStringSerializer() {
+//        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+//        return stringRedisSerializer;
+//    }
+//
+//    @Bean(name = "redisTemplate")
+//    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf, RedisSerializer redisSerializer) {
+//        RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
+//        redisTemplate.setConnectionFactory(cf);
+//        redisTemplate.setDefaultSerializer(redisSerializer);
+//        return redisTemplate;
+//    }
+//
+//    @Bean
+//    public CacheManager cacheManager() {
+//        return new RedisCacheManager(redisTemplate(redisConnectionFactory(), redisStringSerializer()));
+//    }
 }
--- src/main/java/com/hendisantika/rediscache/entity/User.java
+++ src/main/java/com/hendisantika/rediscache/entity/User.java
@@ -52,13 +52,5 @@ public class User implements Serializable {
         this.password = password;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
 
