22d327601dfe6a8fd3d2bf4efdf9328e64568db0
Hendi Santika <hendisantika@gmail.com>
2018-10-06 09:27:48 +0700

    #17 - Init All Users Data

--- src/main/java/com/hendisantika/rediscache/RedisCacheApplication.java
+++ src/main/java/com/hendisantika/rediscache/RedisCacheApplication.java
@@ -1,9 +1,13 @@
 package com.hendisantika.rediscache;
 
+import com.hendisantika.rediscache.entity.User;
+import com.hendisantika.rediscache.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RedisCacheApplication extends SpringBootServletInitializer {
@@ -16,4 +20,20 @@ public class RedisCacheApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(RedisCacheApplication.class, args);
 	}
+
+    @Bean
+    CommandLineRunner initData(UserRepository userRepository) {
+        return args -> {
+            userRepository.save(new User(1L, "Uzumaki Naruto", "123456"));
+            userRepository.save(new User(2L, "Uchiha Sasuke", "123456"));
+            userRepository.save(new User(3L, "Haruno Sakura", "123456"));
+            userRepository.save(new User(4L, "Hatake Kakashi", "123456"));
+            userRepository.save(new User(5L, "Sarutobi Hiruzen", "123456"));
+            userRepository.save(new User(6L, "Sarutobi Asuma", "123456"));
+            userRepository.save(new User(7L, "Sarutobi Asuma", "123456"));
+            userRepository.save(new User(8L, "Minato Namikaze", "123456"));
+            userRepository.save(new User(9L, "Uzumaki Kushina", "123456"));
+            userRepository.save(new User(10L, "Yamanaka Ino", "123456"));
+        };
+    }
 }
