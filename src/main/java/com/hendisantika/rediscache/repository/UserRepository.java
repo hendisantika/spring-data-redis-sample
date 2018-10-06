09974ea937d7918eb58af8f1dce6f4156e792d70
Hendi Santika <hendisantika@gmail.com>
2018-10-06 09:20:46 +0700

    #15 - Add User Repository

---
+++ src/main/java/com/hendisantika/rediscache/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.hendisantika.rediscache.repository;
+
+import com.hendisantika.rediscache.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : redis-cache
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/10/18
+ * Time: 09.19
+ * To change this template use File | Settings | File Templates.
+ */
+public interface UserRepository extends CrudRepository<User, Long> {
+    List<User> findAll();
+}
